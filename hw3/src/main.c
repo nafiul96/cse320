75d0f4907c3517c8b88ecfec12b7d504fac944d0
nafiul azim <mazim@cs.stonybrook.edu>
2018-10-26 18:46:32 -0400

    passed all test cases, ready to submit

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -4,39 +4,18 @@
 int main(int argc, char const *argv[]) {
      sf_mem_init();
 
-    // double* ptr = sf_malloc(sizeof(double));
+    double* ptr = sf_malloc(sizeof(double));
 
-     //*ptr = 320320320e-320;
-
-
-
-     //printf("%f\n", *ptr);
+     *ptr = 320320320e-320;
 
 
-     //sf_free(ptr);
 
-     void *ptr[10];
-
-     for(int i=0; i<10; i++){
-        ptr[i] = sf_malloc(80);
-     }
+     printf("%f\n", *ptr);
 
 
-     for(int i=0; i<10; i++){
-        ptr[i] = sf_realloc(ptr[i],50);
-     }
+     sf_free(ptr);
 
 
-     for(int i=0; i<10;i++){
-
-        if(i%4 == 0){
-            sf_free(ptr[i]);
-        }
-     }
-
-
-     sf_show_heap();
-     //sf_free(ptr[0]);
     sf_mem_fini();
 
 
