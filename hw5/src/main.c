127685b870bf2adaa345e89f052dc3d65a1a5edd
nafiul azim <mdnafiul.azim@stonybrook.edu>
2018-11-27 06:19:34 -0500

    csapp file was brought, part 1 server init passing

--- hw5/src/main.c
+++ hw5/src/main.c
@@ -1,9 +1,27 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "debug.h"
 #include "client_registry.h"
 #include "transaction.h"
 #include "store.h"
+#include "server.h"
+
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include "csapp.h"
+#include <pthread.h>
+
 
 static void terminate(int status);
+
+void handle_hup(int status);
+
+
+
 
 CLIENT_REGISTRY *client_registry;
 
@@ -11,6 +29,43 @@ int main(int argc, char* argv[]){
     // Option processing should be performed here.
     // Option '-p <port>' is required in order to specify the port number
     // on which the server should listen.
+    /**
+     * Code for getting the port number
+     */
+    int opt, port;
+
+
+    if(argc==1){
+    fprintf(stderr, "usage: -p <port number>\n");
+    exit(EXIT_FAILURE);
+}
+
+
+while((opt=getopt(argc, argv, "p:"))  != -1){
+
+    switch(opt){
+
+        case 'p':
+
+            if(optarg == NULL){
+                fprintf(stderr, "Usage: -p <port number>\n");
+                exit(EXIT_FAILURE);
+            }
+            port = atoi(optarg);
+            if(port <= 0){
+                fprintf(stderr, "Usage: -p <port number>\n");
+                exit(EXIT_FAILURE);
+            }
+        break;
+
+        case'?':
+            fprintf(stderr, "Usage: -p <port number>\n");
+            exit(EXIT_FAILURE);
+
+    }
+}
+
+
 
     // Perform required initializations of the client_registry,
     // transaction manager, and object store.
@@ -23,6 +78,29 @@ int main(int argc, char* argv[]){
     // run function xacto_client_service().  In addition, you should install
     // a SIGHUP handler, so that receipt of SIGHUP will perform a clean
     // shutdown of the server.
+    /**
+     * The code for sighup
+     */
+    struct sigaction sa;
+
+    sa.sa_handler = &handle_hup;
+    //sigfillset(&sa.sa_mask);
+    if( sigaction(SIGHUP, &sa, NULL) == -1){
+        perror("Error: SIGHUP handler setup error.");
+    }
+
+    int listenfd, connfd;
+    socklen_t clientlen = sizeof(struct sockaddr_in);
+    struct sockaddr_in clientaddr;
+    pthread_t tid;
+    listenfd = Open_listenfd(port);
+    while(1){
+        connfd = Accept(listenfd, (SA *) &clientaddr, &clientlen);
+        pthread_create(&tid,NULL,xacto_client_service, &connfd);
+    }
+
+
+
 
     fprintf(stderr, "You have to finish implementing main() "
 	    "before the Xacto server will function.\n");
@@ -37,7 +115,7 @@ void terminate(int status) {
     // Shutdown all client connections.
     // This will trigger the eventual termination of service threads.
     creg_shutdown_all(client_registry);
-    
+
     debug("Waiting for service threads to terminate...");
     creg_wait_for_empty(client_registry);
     debug("All service threads terminated.");
@@ -49,4 +127,17 @@ void terminate(int status) {
 
     debug("Xacto server terminating");
     exit(status);
+}
+
+
+
+
+void handle_hup(int status){
+
+    while((waitpid(-1,&status,0)>0));
+    //kill(SIGTERM,status);
+    //pthread_join(status, NULL);
+    pthread_kill(status, SIGTERM);
+    //exit(EXIT_SUCCESS);
+
 }
