add4d48135b66868e76e112e5f015dcbdc57752f
nafiul azim <mdnafiul.azim@stonybrook.edu>
2018-12-02 07:17:47 -0500

    thread has multidef

--- hw5/src/protocol.c
+++ hw5/src/protocol.c
@@ -15,15 +15,15 @@
 // int proto_send_packet(int fd, XACTO_PACKET *pkt, void *data){
 
 
-//    // int size = pkt->size;
+//     int size = pkt->size;
 //     pkt->size = htonl(pkt->size);
 //     pkt->timestamp_sec = htonl(pkt->timestamp_sec);
 //     pkt->timestamp_nsec = htonl(pkt->timestamp_nsec);
-//     if(    rio_writen(fd,pkt,sizeof(XACTO_PACKET)) <= 0){
+//     if(    rio_writen(fd,pkt,sizeof(XACTO_PACKET)) < 0){
 //         return -1;
 //     }
-//     if(pkt->size >0){
-//         if ( rio_writen(fd, data, pkt->size) <= 0){
+//     if(size >0){
+//         if ( rio_writen(fd, data, pkt->size) < 0){
 //             return -1;
 //         }
 
@@ -41,12 +41,12 @@
 //         return -1;
 //     }
 
-//     int size = pkt->size;
+//     //int size = pkt->size;
 //     pkt->size = ntohl(pkt->size);
 //     pkt->timestamp_sec = ntohl(pkt->timestamp_sec);
 //     pkt->timestamp_nsec = ntohl(pkt->timestamp_nsec);
 
-//     if( size >0){
+//     if( pkt->size >0){
 //         *datap = malloc(  sizeof(pkt->size));
 //         if( rio_readn(fd, *datap, pkt->size) <= 0 ){
 //             return -1;
--- hw5/src/server.c
+++ hw5/src/server.c
@@ -1,61 +1,84 @@
-#include "server.h"
-#include "csapp.h"
-#include "client_registry.h"
-#include "protocol.h"
-#include "transaction.h"
-#include "protocol.h"
 
-void *xacto_client_service(void *arg){
+// #include "csapp.h"
+// #include "client_registry.h"
+// #include "protocol.h"
+// #include "transaction.h"
+// #include "protocol.h"
+// #include "debug.h"
+// #include "server.h"
+// #include <pthread.h>
 
-    int connfd = *((int *)arg);
-    Free(arg);
-    Pthread_detach(pthread_self());
-
-    creg_register(client_registry, connfd);
-    //TRANSACTION *trans = trans_create();
-
- // XACTO_NO_PKT,  // Not used
- //    XACTO_PUT_PKT, XACTO_GET_PKT, XACTO_DATA_PKT, XACTO_COMMIT_PKT,
- //    XACTO_REPLY_PKT
-
-    while(1){
-
-        XACTO_PACKET *pkt = (XACTO_PACKET *)Malloc( sizeof (XACTO_PACKET));
-        void **data = Calloc(1,sizeof(int));
-        if( proto_recv_packet(connfd,pkt,data) >0){
-
-            // switch(pkt->type){
-            //     XACTO_PUT_PKT:
-            //     debug("[%d] PUT packet received",connfd);
-            //     //debug("[%d] PUT packet received",connfd);
-            //     //debug("[%d] PUT packet received",connfd);
+// /**
+//  * === DO NOT MODIFY THIS FILE ===
+//  * If you need some other prototypes or constants in a header, please put them
+//  * in another header file.
+//  *
+//  * When we grade, we will be replacing this file with our own copy.
+//  * You have been warned.
+//  * === DO NOT MODIFY THIS FILE ===
+//  */
+// //#ifndef SERVER_H
+// //#define SERVER_H
 
 
-            //     break;
-            //     XACTO_GET_PKT:
-            //     break;
-            //     XACTO_COMMIT_PKT:
-            //     break;
+// /*
+//  * Client registry that should be used to track the set of
+//  * file descriptors of currently connected clients.
+//  */
+// extern CLIENT_REGISTRY *client_registry;
 
-            // }
+// /*
+//  * Thread function for the thread that handles client requests.
+//  *
+//  * @param  Pointer to a variable that holds the file descriptor for
+//  * the client connection.  This pointer must be freed once the file
+//  * descriptor has been retrieved.
+//  */
+// void *xacto_client_service(void *arg){
+
+//     int connfd = *((int *)arg);
+//     Free(arg);
+//     Pthread_detach(pthread_self());
+//     debug("use %d", connfd);
+//     fprintf(stderr, "%d\n",connfd );
+//     creg_register(client_registry, connfd);
+// //     //TRANSACTION *tp = trans_create();
+
+// //     while(1){
+
+// //         XACTO_PACKET *pkt = (XACTO_PACKET *)Malloc( sizeof(XACTO_PACKET));
+// //         void **datap = Calloc(1,sizeof(int));
+// //         if(proto_recv_packet(connfd, pkt, datap) >0 ){
 
 
-        }
+// // // XACTO_NO_PKT,  // Not used
+// // //     XACTO_PUT_PKT, XACTO_GET_PKT, XACTO_DATA_PKT, XACTO_COMMIT_PKT,
+// // //     XACTO_REPLY_PKT
+
+// //             switch(pkt->type){
+
+// //                 case XACTO_PUT_PKT:
+// //                 debug("Put request has been received from %d", connfd);
+// //                 break;
+// //                 case XACTO_GET_PKT:
+// //                 debug("Get request has been received form %d", connfd);
+// //                 break;
+// //                 case XACTO_COMMIT_PKT:
+// //                 debug("Commit request has been received form %d", connfd);
+// //                 Close(connfd);
+// //                 break;
+
+// //             }
+
+
+// //         }
+
+
+// //     }
 
 
 
-    }
+//     return NULL;
+// }
 
-
-}
-
-
-
-
-
-
-
-
-
-
-
+// //#endif
